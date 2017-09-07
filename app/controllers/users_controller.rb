9dcb3d61657b8e8dda0668ecc4340b503cf4117b
Jingjing Duan <jingjing.duan@airbnb.com>
2017-09-06 22:14:13 -0700

    redirect after successfully signing up a user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,7 +10,8 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-      # handle a successful save
+      # or redirect_to user_url(@user)
+      redirect_to @user
     else
       render 'new'
     end
